a7d591b4b0d4ef9d9bbda536e6e51d9c03932b35
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2019-07-19 18:41:43 +0900

    Update 조이스틱.cpp

--- 프로그래머스/조이스틱.cpp
+++ 프로그래머스/조이스틱.cpp
@@ -6,75 +6,59 @@ using namespace std;
 
 int solution(string name) {
     int answer = 0;
-    float size = name.length();
+    int size = name.length();
     int cursor = 0;
-    int temp = 0;
-    int cursor_move[2] = {0, 0};
     int count = 0;
-    int check_mv = 0;
-    int i = 0;
+    int temp = 0;
     vector<char> name_t;
+    vector<int> index;
     vector<pair<int, int>> check;
     for(int i = 0; i < size; i++){
         name_t.push_back(name[i]);
-        if(name[i] == 'A'){
+        if(name[i] == 'A')
             check.push_back(make_pair(1, 0));
-            if(i >= size/2.0)
-                cursor_move[1]++;
-            else            
-                cursor_move[0]++;
-        }
         else{
             temp = (abs(name[i] - 65) >= abs(name[i] - 91) ? abs(name[i] - 91) : abs(name[i] - 65));
             check.push_back(make_pair(0, temp));
         }
     }
     //A의 위치에 따라 커서 이동방향 선택
-    
-   check_mv = (cursor_move[1] > cursor_move[0]) ?  0 : 1;
-    
-        
     while(1){
-        //문자열 전부 탐색 완료시 반복문 종료
         if(count == size)
             break;
-        
         count++;
-        //뒤로 탐색하여 커서가 시작 -> 끝으로 넘어가는 경우
-        if(i == -1){
-            cursor = size;
-            i = size - 1;
+        int min[2] = {size, 0};
+        for(int i = 0; i < size; i++){
+            if(check[i].first != 1){
+                temp = (abs(i - cursor) > cursor + (size - i)) ? cursor + (size - i) : (abs(i - cursor));
+                if(temp < min[0]){
+                    min[0] = temp;
+                    min[1] = i;
+                }
+            }
         }
-        //입력 문자열이 A인 경우, 위치만 변경 후 반복문 계속
-        if(check[i].first == 1){
-            if(check_mv == 1)
-                i--;
-            else
-                i++;
+        if(count != 1 && min[1] == 0)
             continue;
-        }
-        else{
-            
-                if(check_mv == 1){
-                    temp = abs(cursor - i);
-                    check[i].second += temp;
-                    check[i].first = 1;
-                    cursor = i--;
-                }
-                else{
-                    temp = abs(i - cursor);
-                    check[i].second += temp;
-                    check[i].first = 1;
-                    cursor = i++;
-                }
-        }
-        
+        index.push_back(min[1]);
+        check[min[1]].first = 1;
+        cursor = min[1];
     }
+        
     for(int i = 0; i < size; i++){
         answer += check[i].second;
+    }
+    cursor = index[0];
+    answer += index[0];
+    for(int i = 0; i < index.size(); i++)
+        printf("%d", index[i]);
+    for(int i = 1; i < index.size(); i++){
+        
+        temp = (abs(index[i] - cursor) > abs(cursor + (size - index[i])) ) ? abs(cursor + (size - index[i])) : abs(index[i] - cursor);
+        
+        answer += temp;
+        cursor = index[i];
     }
     //A = 65;
     //Z = 90
     return answer;
 }
-#테스트케이스 11 안됨. 혼합결과 생각.
