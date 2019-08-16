262ffe298630e9c0808a1c0002bf58adf514e4ed
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2019-08-16 17:10:29 +0900

    Update 짝지어 제거하기(시간복잡도 X).cpp

--- 프로그래머스/짝지어 제거하기(시간복잡도 X).cpp
+++ 프로그래머스/짝지어 제거하기(시간복잡도 X).cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include<string>
+#include <vector>
 using namespace std;
 
 int solution(string s)
 {
     int answer = 0;
-    int i = 0, check = 0;;
-    while(s.size() >= 1){
-        if(i >= s.size())
-            i = 0;
-        if(s[i] == s[i+1]){
-            check = 0;       
-            s.erase(i, 2);
-        }
-        else{
-            i++;
-            check++;
-            if(check >= s.size())
+    int i = 0, check = 1;
+    vector<int> index;
+    index.clear();
+    while(check != 0){
+        check = 0;
+        for(int i = 0; i < s.size(); i++)
+            cout << s[i];
+        cout << endl;
+        for(int i = 0; i < s.size()-1; i++){
+            if(i+1 >= s.size())
                 break;
+            if(s[i] == s[i+1]){
+                index.push_back(i);
+                check = 1;
+                i += 1;
+                continue;
+            }
         }
+        for(int i = index.size()-1 ; i >= 0; i--){
+            s.erase(index[i], 2);
+        }
+        index.clear();
+        
     }
     if(s.size() == 0)
         answer = 1;
