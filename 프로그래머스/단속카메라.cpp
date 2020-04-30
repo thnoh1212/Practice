3aaebf21334f76214c2d7ab7688bb07a7a3e53c0
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2020-04-30 17:50:16 +0900

    Update 단속카메라.cpp

--- 프로그래머스/단속카메라.cpp
+++ 프로그래머스/단속카메라.cpp
@@ -5,16 +5,19 @@
 
 using namespace std;
 
+bool cmp(vector<int> a, vector<int> b){
+    return a[1] <= b[1];
+}
+
 int solution(vector<vector<int>> routes) {
-    int answer = 1;
-    sort(routes.begin(), routes.end());
-    int range = routes[0][1];
+    int answer = 0;
+    sort(routes.begin(), routes.end(), cmp);
+    int range = -30000;
     
-    for(int i = 0; i < routes.size(); i++){
-        if(range > routes[i][1]) range = routes[i][1];
-        if(range < routes[i][0]){
+    for(auto &route : routes){// 배열 auto로 사용
+        if(range < route[0]){
             answer += 1;
-            range = routes[i][1];
+            range = route[1];
         }
     }
     return answer;
