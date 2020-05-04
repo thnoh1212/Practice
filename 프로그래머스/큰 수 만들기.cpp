6f71269e70d556a92ae9cf3b6b7db1fcc78e2b0f
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2020-05-04 15:12:14 +0900

    Update 큰 수 만들기.cpp

--- 프로그래머스/큰 수 만들기.cpp
+++ 프로그래머스/큰 수 만들기.cpp
@@ -1,28 +1,22 @@
 #include <string>
-#include <vector>
-#include <algorithm>
-#include <iostream>
+
 using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
     int need = number.length() - k; // 정답으로 제출할 문자열의 길이 계산
     int max = 0, cnt = 0;
-    int j;
-    vector<int> temp;
     for (int i = need; i>0; i--) { // 정답 문자열의 길이만큼 반복
         max = 0;
         cnt = 0;
-        for (j = 0; j <= number.length() - i; j++) { // 삭제할 수 있는 값의 범위중 최댓값 탐색
+        for (int j = 0; j <= number.length() - i; j++) { // 삭제할 수 있는 값의 범위중 최댓값 탐색
             if (max<number[j]) {
                 max = number[j];
                 cnt = j;
             }
         }
         number.replace(0,cnt+1,""); // 최댓값 앞에있는 값들은 전부 삭제
-        temp.push_back(max); //최대값 삽입
+        answer += to_string(max - 48); //최대값 삽입
     }
-    for (int i = 0; i<temp.size(); i++)
-        answer += temp[i];
     return answer;
 }
