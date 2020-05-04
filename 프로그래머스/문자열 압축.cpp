6b5eaf305bc410e8d28ef78f0d1dab1c5dc33647
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2020-05-04 22:16:43 +0900

    Update 문자열 압축.cpp

--- 프로그래머스/문자열 압축.cpp
+++ 프로그래머스/문자열 압축.cpp
@@ -4,26 +4,24 @@
 using namespace std;
 
 int solution(string s) {
-    int answer = 1000;
-    
+    int answer = s.length();
     for(int i = 1; i <= s.length()/2; i++){
         string word = s.substr(0, i);
         string remainder = s;
         int cnt = 1;
         remainder.erase(0,i);
         string t_answer = "";
-        
         while(remainder.length() >= i){
-            string temp_2 = remainder.substr(0,i);
+            string temp = remainder.substr(0,i);
             remainder.erase(0, i);
-            if(word == temp_2)
+            if(word == temp)
                 cnt++;
             else{
                 if(cnt != 1)
                     t_answer += to_string(cnt) + word;
                 else
                     t_answer += temp;
-                word = temp_2;
+                word = temp;
                 cnt = 1;
             }
         }
@@ -32,16 +30,12 @@ int solution(string s) {
             t_answer += to_string(cnt) + word;
         else
             t_answer += word;
-        
         // 잘리지 않고 remainder 남아있는 값 추가
         if(remainder.length() != 0)
             t_answer += remainder;
-        
         // 정답과 길이비교
         if(answer > t_answer.length())
             answer = t_answer.length();      
     }
-    if(answer > s.length())
-        answer = s.length();
     return answer;
 }
