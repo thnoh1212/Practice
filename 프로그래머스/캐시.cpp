780bd8d9befda8923ba2a3a6163ded3a3c4a496b
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2020-05-13 19:08:28 +0900

    Create 캐시.cpp

---
+++ 프로그래머스/캐시.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities) {
+    int answer = 0;
+    int cnt = 0;
+    vector<string> cache;
+    
+    if(cacheSize == 0)// 캐시 사이즈가 0인경우는 모든 경우가 cache miss
+        return cities.size() * 5;
+    
+    for(int i = 0; i < cities.size(); i++){
+        transform(cities[i].begin(), cities[i].end(), cities[i].begin(), ::tolower);
+    }
+    
+    for(auto a = cities.begin(); a != cities.end(); a++){
+        if(cnt == 0){ // 시작
+            cache.push_back(*a);
+            answer += 5;
+            cnt++;
+        }
+        else{ // 그 외
+            int flag = -1;
+            for(int i = 0; i < cnt; i++){
+                if(cache[i] == *a){
+                    flag = i;
+                    break;
+                }
+            }
+            if(flag != -1){ // flag == -1 인 경우를 제외하고는 cache hit. 이 경우 이번에 사용한 값을 cache의 맨 마지막으로 이동
+                answer += 1;
+                cache.erase(cache.begin() + flag);
+                cache.push_back(*a);
+            }
+            else{// cache miss인 경우
+                if(cnt < cacheSize){// 캐시 용량이 남아있는 경우 cache에 이번 값 추가
+                    cache.push_back(*a);
+                    answer += 5;
+                    cnt++;
+                }
+                else{// 캐시 용량이 가득 찬 경우 가장 마지막에 사용한 값 삭제 후 이번값 추가
+                    cache.push_back(*a);
+                    cache.erase(cache.begin());
+                    answer += 5;
+                }
+            }
+        }
+    }
+    return answer;
+}
