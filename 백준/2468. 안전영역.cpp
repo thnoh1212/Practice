851d7386b6115bb8fb219bd0b5b79e48c1ed2b71
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2020-06-08 18:23:25 +0900

    Create 2468. 안전영역.cpp

---
+++ 백준/2468. 안전영역.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int moves[4][2] = { {1,0}, {0,1}, {-1, 0}, {0, -1} };
+int region[100][100];
+
+void bfs(bool map[][100], int min, int i, int j, int N) {
+
+	queue<pair<int, int>> temp;
+	temp.push(make_pair(i, j));
+	map[i][j] = true;
+	while (!temp.empty()) {
+		int cur_i = temp.front().first;
+		int cur_j = temp.front().second;
+		temp.pop();
+		for (int i = 0; i < 4; i++) {
+			int next_i = cur_i + moves[i][0];
+			int next_j = cur_j + moves[i][1];
+
+			if (next_i >= 0 && next_i < N && next_j >= 0 && next_j < N) {
+				if (region[next_i][next_j] > min && !map[next_i][next_j]) {
+					map[next_i][next_j] = true;
+					temp.push(make_pair(next_i, next_j));
+				}
+			}
+
+		}
+	}
+
+
+}
+
+int main()
+{
+	int N;
+	int min = 100;
+	int max = 0;
+	int answer = 1;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> region[i][j];
+			if (min > region[i][j])
+				min = region[i][j];
+			if (max < region[i][j])
+				max = region[i][j];
+		}
+	}
+
+	while (min != max) {
+		bool map[100][100] = { false };
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(!map[i][j] && region[i][j] > min){
+					bfs(map, min, i, j, N);
+					cnt++;
+				}
+			}
+		}
+		if (cnt > answer)
+			answer = cnt;
+		min += 1;
+	}
+	cout << answer;
+}
