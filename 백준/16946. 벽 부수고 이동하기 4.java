c118d5ef6002d3c5ca5e5aac42ebacfbae123157
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2020-08-25 11:15:38 +0900

    Update 16946. 벽 부수고 이동하기 4.java

--- 백준/16946. 벽 부수고 이동하기 4.java
+++ 백준/16946. 벽 부수고 이동하기 4.java
@@ -1,3 +1,15 @@
+/*
+ * 벽 부수고 이동하기4(16946)
+ * 
+ * 모든 벽에 대하여 BFS를 실행하면 시간초과 발생
+ * 벽으로 인해 나눠진 영역들의 넓이를 미리 계산한 후
+ * 각 벽에 대하여 주위 4방향을 탐색, 인접한 영역의 넓이를 모두 더해준 후 현재 위치의 벽이 부서졌으므로 +1 추가
+ * 
+ * 벽의 두 면이 하나의 영역과 인접해 있는 경우 4방향을 탐색하여 더해준다면 하나의 영역이 두번 더해지는 오류 발생
+ * 이를 해결하기 위해 MAP배열에 각 영역을 2 이상의 숫자로 마킹하여 영역을 나타내고, 해당 영역의 넓이를 chk배열에 저장
+ * 그 후 벽을 기준으로 탐색하는 과정에서 Set을 사용하여 중복값을 제거.
+ */
+
 import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -7,7 +19,7 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main16946 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -26,7 +38,6 @@ public class Main {
 		}
 		int cnt = 2;
 		int[] chk = new int[1000002];
-		
 		Queue<Point> que = new LinkedList<>();
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
@@ -53,7 +64,6 @@ public class Main {
 				}
 			}
 		}
-		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				if(map[i][j] == 1) {
