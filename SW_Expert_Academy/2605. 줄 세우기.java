fca708d2e4fee62b104ddb10bede7f895eeae81c
thnoh1212 <40189618+thnoh1212@users.noreply.github.com>
2020-09-23 11:45:31 +0900

    Create 2605. 줄 세우기.java

---
+++ SW_Expert_Academy/2605. 줄 세우기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String[] temp = br.readLine().split(" ");
+		ArrayList<Integer> test = new ArrayList<>();
+		for(Integer i = 0; i < N; i++) {
+			Integer num = Integer.parseInt(temp[i]);
+			num = i - num;
+			test.add(num, i+1);
+		}
+		
+		for(int i = 0; i < test.size(); i++)
+			System.out.print(test.get(i) + " ");
+			
+		
+	}
+}
